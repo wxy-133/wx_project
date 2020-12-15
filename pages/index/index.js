a0999d7606ab6164b07aaf28fbc21cf4f5505d7e
wxy-133 <w15279621208@163.com>
2020-12-16 00:05:47 +0800

    request优化(promise)

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,4 +1,5 @@
 //Page Object
+import {request} from '../../request/index'
 Page({
   data:{
     //轮播图数组
@@ -7,14 +8,20 @@ Page({
   //options(Object)
   onLoad: function(options) {
      //发送异步请求
-     wx.request({
-       url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-       success:(result)=>{
-         this.setData({
-           swiperList:result.data.message
-         })
-       }
-     })
+    //  wx.request({
+    //    url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //    success:(result)=>{
+    //      this.setData({
+    //        swiperList:result.data.message
+    //      })
+    //    }
+    //  })
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+    .then(result=>{
+      this.setData({
+               swiperList:result.data.message
+             })
+    })
   }
 
 });
