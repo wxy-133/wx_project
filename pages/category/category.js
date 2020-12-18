2b8e7b798a8340d43332b63f5ed4f7984140658f
wxy-133 <w15279621208@163.com>
2020-12-19 00:20:10 +0800

    12/19分类tab完成

--- pages/category/category.js
+++ pages/category/category.js
@@ -1,66 +1,40 @@
 // pages/category/category.js
+import {request} from '../../request/index.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+      leftMenuList:[],
+      rightMenuList:[],
+      currentIndex:0
   },
-
+ Cates:[],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getCates();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  getCates  () {
+    request({url:'https://api-hmugo-web.itheima.net/api/public/v1/categories'})
+    .then(result=>{
+        this.Cates=result.data.message;
+        let leftMenuList=this.Cates.map(v=>v.cat_name);
+        let rightMenuList=this.Cates[0].children;
+        this.setData({
+          leftMenuList,
+          rightMenuList
+        })
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleItemTab(e){
+   const {index}=e.currentTarget.dataset;
+   let rightMenuList=this.Cates[index].children;
+   this.setData({
+    currentIndex:index,
+    rightMenuList
+  })
   }
 })
