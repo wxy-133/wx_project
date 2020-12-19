8152b565251293089059d637fad745bc2a12edd4
wxy-133 <w15279621208@163.com>
2020-12-20 00:18:44 +0800

    11/20分类页面完成

--- pages/category/category.js
+++ pages/category/category.js
@@ -8,19 +8,37 @@ Page({
   data: {
       leftMenuList:[],
       rightMenuList:[],
-      currentIndex:0
+      currentIndex:0,
+      scrollTop:0
   },
  Cates:[],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getCates();
+    // this.getCates();
+    const Cates=wx.getStorageSync('cates');
+    if(!Cates){
+      this.getCates();
+    }else{
+      if(Date.now()-Cates.time>1000*10){
+        this.getCates();
+      }else{
+       this.Cates=Cates.data;
+       let leftMenuList=this.Cates.map(v=>v.cat_name);
+       let rightMenuList=this.Cates[0].children;
+       this.setData({
+         leftMenuList,
+         rightMenuList
+       })
+      }
+    }
   },
   getCates  () {
     request({url:'https://api-hmugo-web.itheima.net/api/public/v1/categories'})
     .then(result=>{
         this.Cates=result.data.message;
+        wx.setStorageSync("cates",{time:Date.now(),data:this.Cates});
         let leftMenuList=this.Cates.map(v=>v.cat_name);
         let rightMenuList=this.Cates[0].children;
         this.setData({
@@ -34,7 +52,8 @@ Page({
    let rightMenuList=this.Cates[index].children;
    this.setData({
     currentIndex:index,
-    rightMenuList
+    rightMenuList,
+    scrollTop:0
   })
   }
 })
