72aa3d1aa4b6fc40a08169b13c8b81d9a932a5b4
wxy-133 <w15279621208@163.com>
2020-12-22 00:12:51 +0800

    12/21商品列表页下拉刷新

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,4 +1,7 @@
 // pages/goods_list/goods_list.js
+import {
+  request
+} from '../../request/index.js';
 Page({
 
   /**
@@ -20,21 +23,60 @@ Page({
         value: "价格",
         isActive: false
       }
-    ]
+    ],
+    goodsList: []
   },
+  QueryParams: {
+    query: "",
+    pagenum: 1,
+    pagesize: 10
+  },
+  totalPages:1,
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    // console.log(options)
+    this.QueryParams.cid = options.cid;
+    this.getGOodsList();
   },
-  handelTabsItemChange(e){
+  async getGOodsList() {
+    const res = await request({
+      url: "/goods/search",
+      data: this.QueryParams
+    });
+    const result = res.data.message;
+    const total = result.total;
+    this.totalPages=Math.ceil(total/this.QueryParams.pagesize);
+    // console.log(this.totalPages)
+
+    //  console.log(result)
+    this.setData({
+      goodsList:[...this.data.goodsList,...result.goods]
+    })
+  },
+  handelTabsItemChange(e) {
     // console.log(e)
-    const {index}=e.detail;
-    let {tabs}=this.data;
-    tabs.forEach((v,i)=>i===index?v.isActive=true:v.isActive=false);
+    const {
+      index
+    } = e.detail;
+    let {
+      tabs
+    } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
     this.setData({
       tabs
     })
+  },
+  onReachBottom(){
+    if(this.QueryParams.pagenum>=this.totalPages){
+      wx-wx.showToast({
+        title: '没有下一页数据',
+      })
+    }else{
+      // console.log('22')
+      this.QueryParams.pagenum++;
+      this.getGOodsList();
+    }
   }
 })
