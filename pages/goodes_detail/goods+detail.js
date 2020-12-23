af5c684b65e4698370d041b246ecb4ef86415e6f
wxy-133 <w15279621208@163.com>
2020-12-24 00:08:28 +0800

    12/23详情页预览大图底部工具栏

--- pages/goodes_detail/goods+detail.js
+++ pages/goodes_detail/goods+detail.js
@@ -1,66 +1,46 @@
 // pages/goodes_detail/goods+detail.js
+import {request} from '../../request/index.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj:{}
   },
-
+GoodsInfo:{
+   
+},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const {goods_id} = options;
+    // console.log(goods_id);
+    this.getGoodsDetail(goods_id);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+ async getGoodsDetail(goods_id){
+ const res=await request({url: '/goods/detail',data:{goods_id}});
+ const goodsObj= res.data.message;
+ this.GoodsInfo= res.data.message;
+//  console.log(goodsObj)
+ this.setData({
+   goodsObj:{
+     goods_name:goodsObj.goods_name,
+     goods_price:goodsObj.goods_price,
+     //部分手机不识别webp格式
+     //后端修改或者临时修改(格式存在)
+     goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,'.jpg'),
+     pics:goodsObj.pics
+   }
+ })
+ },
+ handlePrevImage(e){
+   const urls=this.GoodsInfo.pics.map(v=>v.pics_mid);
+   const current=e.currentTarget.dataset.url;
+   wx.previewImage({
+     current:current,
+     urls: urls,
+   })
+ }
 })
