8821c8e4a4ac9df075c6b928475597ac18692c45
wxy-133 <w15279621208@163.com>
2021-01-16 23:50:59 +0800

    1/16搜索页面完成

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,66 +1,48 @@
 // pages/search/search.js
+import {
+  request
+} from '../../request/index'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods:[],
+    isFocus:false,
+    inputValue:""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  TimeId:-1,
+  handleInput(e) {
+    const {value} = e.detail;
+    if (!value.trim()) {
+      this.setData({
+        goods:[],
+        isFocus:false
+      })
+      return;
+    }
+    this.setData({
+      isFocus:true
+    }),
+    clearTimeout(this.TimeId);
+    this.TimeId=setTimeout(()=>{
+      this.qsearch(value)
+    },1000)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async qsearch(query) {
+    const result=await request({url:"/goods/qsearch",data:{query}});
+    const res=result.data.message;
+    // console.log(res)
+    this.setData({
+      goods:res
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleCancel(){
+    this.setData({
+      inputValue:"",
+      isFocus:false,
+      goods:[]
+    })
   }
 })
